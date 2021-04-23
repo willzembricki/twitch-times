59020a652b29fda96479ae895ec9d7036d1921cd
William Zembricki <wzembricki@gmail.com>
2021-04-23 09:31:13 -0400

    Final Front End Commit

--- src/components/NewBlogForm.js
+++ src/components/NewBlogForm.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+
 function NewBlogForm({ onAddBlog }) {
+  //Setting states for controlled forms
   const [authorInput, setAuthorInput] = useState("");
   const [blogTitle, setBlogTitle] = useState("");
   const [inputUrl, setInputUrl] = useState("");
   const [blogContent, setBlogContent] = useState("");
+  // handles a new blog being added
   function formSubmission(e) {
     e.preventDefault();
     const formData = {
@@ -14,7 +17,7 @@ function NewBlogForm({ onAddBlog }) {
       blogUpVote: 0,
       blogDownVote: 0,
     };
-    fetch(`http://localhost:4002/articles?_embed=articleComments`, {
+    fetch(`http://localhost:4002/articles`, {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -22,16 +25,20 @@ function NewBlogForm({ onAddBlog }) {
       body: JSON.stringify(formData),
     })
       .then((res) => res.json())
-      .then((newBlog) => onAddBlog(newBlog));
+      .then((newBlog) => {
+        // this adds a blank array in the front end to stop errors.
+        newBlog.articleComments = [];
+        onAddBlog(newBlog);
+      });
     setAuthorInput("");
     setBlogTitle("");
     setInputUrl("");
     setBlogContent("");
   }
-
+  // Builds the Form
   return (
     <div>
-      <h1>New Blog Form</h1>
+      <h2>New Blog Form</h2>
       <form onSubmit={formSubmission}>
         <input
           onChange={(e) => setAuthorInput(e.target.value)}
